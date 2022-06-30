591c2c66e46f1ebb1f5d490d9d5ea2f9c491c48d
emanuel12012 <emanuel.cafloriano@gmail.com>
2022-06-30 10:06:08 -0300

    Primeiro Commit

---
+++ src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="login">
+    <h1>Login</h1>
+    <input type="text" placeholder="CPF ou Email">
+    <br>
+    <input type="password" placeholder="Senha">
+    <br>
+    <div class="btn-login">
+        <button>Login</button>
+    </div>
+    <p>
+        Você não tem conta?
+        <a href=""></a>Crie uma conta</a>
+    </p>
+</div>
+</template>
+
+<script>
+
+export default {
+    name: "login",
+    data() {
+        return{};
+    },
+    methods: {}
+};
+
+</script>
+<style scoped>
