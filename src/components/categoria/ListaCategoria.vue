11a32952a49548c29f4fcd654f95ea5d15a25cd6
emanuel12012 <emanuel.cafloriano@gmail.com>
2022-07-07 10:15:26 -0300

    Commit

---
+++ src/components/categoria/ListaCategoria.vue
@@ -0,0 +1,55 @@
+<script>
+import { mapState, mapStores, mapActions } from "pinia";
+import { useCategoryStore } from "@/stores/category";
+import DataTable from "../template/DataTable.vue";
+export default {
+  components: { DataTable },
+  data() {
+    return {
+      columns: [
+        { label: "ID", field: "id" },
+        { label: "Descrição", field: "description" },
+      ],
+    };
+  },
+  computed: {
+    ...mapStores(useCategoryStore),
+    ...mapState(useCategoryStore, ["categories"]),
+  },
+  methods: {
+    ...mapActions(useCategoryStore, ["getAllCategories", "deleteCategory"]),
+    async deleteItem(category) {
+      try {
+        await this.deleteCategory(category.id);
+        alert("Item excluído com sucesso.");
+      } catch (e) {
+        alert(e);
+      }
+    },
+  },
+  async mounted() {
+    try {
+      await this.getAllCategories();
+    } catch (e) {
+      alert(e);
+    }
+  },
+};
+</script>
+<template>
+  <div class="category-list">
+    <data-table
+      :columns="columns"
+      :items="categories"
+      @edit="$emit('edit', $event)"
+      @delete="deleteItem"
+    />
+  </div>
+</template>
+
+<style scoped>
+.category-list {
+  margin: 3% auto;
+  width: 70%;
+}
+</style>
